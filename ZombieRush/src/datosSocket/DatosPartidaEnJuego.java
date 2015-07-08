efa71732a15cfea0d1f484010543020398d3debe
mpandullo <mpandullo@gmail.com>
2015-07-08 19:59:38 -0300

    broadcast

---
+++ ZombieRush/src/datosSocket/DatosPartidaEnJuego.java
@@ -0,0 +1,8 @@
+package datosSocket;
+
+import java.io.Serializable;
+
+public class DatosPartidaEnJuego implements Serializable {
+	private int[][] matriz;
+
+}
---
+++ ZombieRush/src/servidor/Broadcast.java
@@ -0,0 +1,20 @@
+package servidor;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.List;
+
+public class Broadcast {
+
+	public void broadcastMsg(Object obj, List<Usuario> listaUsuarios)
+			throws IOException {
+
+		for (Usuario usuario : listaUsuarios) {
+			ObjectOutputStream outStream = new ObjectOutputStream(
+					usuario.getSocket.getOutputStream());
+			outStream.writeObject(obj);
+		}
+
+	}
+}
